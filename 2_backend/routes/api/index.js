040a8b2ad69a06e670375aa9c53a6e4e76df834a
javisolercanto <jsolercanto11@gmail.com>
2020-09-16 10:36:49 +0200

    fixed articles bug

--- 2_backend/routes/api/index.js
+++ 2_backend/routes/api/index.js
@@ -3,6 +3,7 @@ var router = require('express').Router();
 router.use('/', require('./users'));
 router.use('/profiles', require('./profiles'));
 router.use('/movies', require('./movies'));
+router.use('/articles', require('./articles'));
 router.use('/tags', require('./tags'));
 
 router.use(function(err, req, res, next){
